834fb0b9b3737b2a170b620b50c033ca41e51558
Gollamudi03 <sindhura.gollamudi@yahoo.com>
2020-11-06 23:23:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+       "My name is Sindhura"
+    end 
+    get '/hometown' do
+      "My hometown is Dettroit, MI."
+    end 
+    get '/favorite-song' do
+      "My favorite song is Savage Love."
+    end 
 end
